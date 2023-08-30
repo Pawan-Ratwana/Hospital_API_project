09b0b671290b3f46f69eac95a0541f371518365b
Pawan-Ratwana <ratwanapawan@gmail.com>
2023-08-30 22:42:56 +0530

    check

--- routes/api/v1/reports.js
+++ routes/api/v1/reports.js
@@ -7,7 +7,7 @@ const reportController = require('../../../controllers/api/v1/report_controller'
 const passport = require('passport');
 
 router.get('/all_reports',  reportController.all_reports);
- 
+
 router.get('/status',  reportController.report_by_status);
 
 module.exports = router;
